59bfe02787d723e24759cd88767fcff87b695ce3
mattn <mattn.jp@gmail.com>
2014-02-19 11:51:38 +0900

    Read stdin

--- play_any.go
+++ play_any.go
@@ -9,17 +9,20 @@ import (
 )
 
 func playURL(url string) error {
+	var cmd *exec.Cmd
 	for _, player := range []string{"ffplay", "avplay"} {
 		f, err := exec.LookPath(player)
 		if err == nil {
 			args := []string{"-autoexit", "-nodisp", url}
-			return exec.Command(f, args...).Run()
+			cmd = exec.Command(f, args...)
+			cmd.Stdin = os.Stdin
+			return cmd.Run()
 		}
 	}
 
 	f, err := exec.LookPath("mplayer")
 	if err == nil {
-		cmd := exec.Command(f, url)
+		cmd = exec.Command(f, url)
 		cmd.Stdin = os.Stdin
 		return cmd.Run()
 	}
